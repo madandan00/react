d5b38bcd0761f1df60a871e6f5d6e8dc60931c2c
madandan00 <1910763812@qq.com>
2017-01-10 17:29:18 +0800

    1,10,3

--- prd/bundle.js
+++ prd/bundle.js
@@ -44,12 +44,20 @@
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
-	'use strict';
-	
 	__webpack_require__(1);
+	(function webpackMissingModule() { throw new Error("Cannot find module \"init\""); }());
+
 
 /***/ },
 /* 1 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	__webpack_require__(2);
+
+/***/ },
+/* 2 */
 /***/ function(module, exports) {
 
 	// removed by extract-text-webpack-plugin
